d21d5172493cf06b3ee724ec886c556be4501e14
Philip <philipcho101@hotmail.com>
2022-05-29 10:10:45 -0700

    Add input form modal for new book entries

--- javascript.js
+++ javascript.js
@@ -7,11 +7,7 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {
-    let bookTitle = prompt('Title','Hobbit');
-    let bookAuthor = prompt('Author', 'JK');
-    let bookPages = prompt('num of pages', 295);
-    let bookRead = prompt('read?', false);
+function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead) {
     const newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
     myLibrary.push(newBook);
 }
@@ -27,8 +23,46 @@ function displayLibrary() {
     }
 }
 
+function displayBook(bookTitle, bookAuthor, bookPages, bookRead) {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+    bookElement.textContent = bookTitle + bookAuthor + bookPages + bookRead;
+    library.appendChild(bookElement);
+}
+
+let button = document.getElementById('modal-opener');
+let modal = document.querySelector('.modal');
+
+button.addEventListener('click', openModal);
+window.addEventListener('click', function(e) {
+    if (e.target == modal) {
+        closeModal();
+    }
+});
+
+function openModal() {
+    modal.style.display = 'block';
+}
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+let form = document.getElementById('book-form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let bookTitle = document.querySelector('.book-title').value;
+    let bookAuthor = document.querySelector('.book-author').value;
+    let bookPages = document.querySelector('.book-pages').value;
+    let bookRead = document.querySelector('.book-read').checked;
+    console.log(bookTitle, bookAuthor, bookPages, bookRead);
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead);
+    displayBook(bookTitle, bookAuthor, bookPages, bookRead);
+})
+
 testBook1 = new Book('ASOIAF','Martin',700,false);
 testBook2 = new Book('LOTD','Tolkien',500,false);
 testBook3 = new Book('OFOTCN','Kesey',300,true);
 testBook4 = new Book('TKAM','Harper',200,true);
 myLibrary.push(testBook1, testBook2, testBook3, testBook4);
+displayLibrary();
